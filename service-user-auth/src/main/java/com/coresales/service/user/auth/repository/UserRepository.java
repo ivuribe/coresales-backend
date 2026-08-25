0bcf7b183fedf4f7586ab39e19603fa31f7fb6f1
Ivan Uribe <ivan.uribec@gmail.com>
2026-08-24 21:57:31 -0500

    Implementacion de JWT - Primera Parte

--- service-user-auth/src/main/java/com/coresales/service/user/auth/repository/UserRepository.java
+++ service-user-auth/src/main/java/com/coresales/service/user/auth/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.coresales.service.user.auth.repository;
 
 import com.coresales.service.user.auth.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+//@Repository //-> no es necesario poner @Repository porque ya está heredando de JpaRepository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
 }
