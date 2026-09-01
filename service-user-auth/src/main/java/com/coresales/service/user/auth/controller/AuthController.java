99429de4940413c33db784cf8f59498ba0087406
Ivan Uribe <ivan.uribec@gmail.com>
2026-08-31 20:58:13 -0500

    Creación del API Gateway

--- service-user-auth/src/main/java/com/coresales/service/user/auth/controller/AuthController.java
+++ service-user-auth/src/main/java/com/coresales/service/user/auth/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("api/auth")
 public class AuthController {
 
     private final UserService userService;
